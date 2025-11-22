ea1fdcacb666fb3cb05679623fe58eb492e0ea8b
Mayank Sharma <mayank27800@gmail.com>
2025-11-22 23:25:27 +0530

    MVP of the marks view page

--- Mayank_ViewMarksFrame.java
+++ Mayank_ViewMarksFrame.java
@@ -1,237 +1,177 @@
-   public static class Mayank_ViewMarksFrame extends JFrame implements ActionListener {
-      JTextField tfFilter;
-      JTextArea ta;
-      JButton btnRefresh;
-      JButton btnBack;
-      JButton btnFilter;
-      String user;
-      String role;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.HashMap;
 
-      public Mayank_ViewMarksFrame(String var1, String var2) {
-         super("View Students & Marks (Mayank)");
-         this.user = var1;
-         this.role = var2;
-         this.tfFilter = new JTextField(20);
-         this.ta = new JTextArea(20, 70);
-         this.ta.setFont(new Font("Monospaced", 0, 12));
-         this.btnRefresh = new JButton("Refresh");
-         this.btnFilter = new JButton("Filter (roll/class)");
-         this.btnBack = new JButton("Back");
-         this.btnRefresh.addActionListener(this);
-         this.btnFilter.addActionListener(this);
-         this.btnBack.addActionListener((var3x) -> {
-            this.dispose();
-            new Yachika_DashboardFrame(var1, var2);
-         });
-         JPanel var3 = new JPanel();
-         var3.add(new JLabel("Filter:"));
-         var3.add(this.tfFilter);
-         var3.add(this.btnFilter);
-         var3.add(this.btnRefresh);
-         var3.add(this.btnBack);
-         this.add(var3, "North");
-         this.add(new JScrollPane(this.ta), "Center");
-         this.setSize(900, 600);
-         this.setLocationRelativeTo((Component)null);
-         this.setVisible(true);
-         this.loadAll("");
-      }
+public class Mayank_ViewMarksFrame extends JFrame implements ActionListener {
+    JTextField tfFilter;
+    JTextArea ta;
+    JButton btnRefresh;
+    JButton btnBack;
+    JButton btnFilter;
+    String user;
+    String role;
 
-      public void actionPerformed(ActionEvent var1) {
-         String var2 = var1.getActionCommand();
-         if (var2.equals("Refresh")) {
-            this.loadAll("");
-         } else if (var2.equals("Filter (roll/class)")) {
-            this.loadAll(this.tfFilter.getText().trim());
-         }
+    // Mock subjects
+    ArrayList<String> subjects = new ArrayList<String>();
 
-      }
+    // Mock students { roll, name, class }
+    ArrayList<String[]> students = new ArrayList<String[]>();
 
-      void loadAll(String var1) {
-         this.ta.setText("");
+    // Mock marks: key = roll + "-" + subject
+    HashMap<String, Integer> marks = new HashMap<String, Integer>();
 
-         try {
-            Connection var2 = MarksManagement.DB.getConnection();
+    public Mayank_ViewMarksFrame(String var1, String var2) {
+        super("View Students & Marks (Mayank - Offline Demo)");
 
-            try {
-               ArrayList var3 = new ArrayList();
-               Statement var4 = var2.createStatement();
+        this.user = var1;
+        this.role = var2;
 
-               try {
-                  ResultSet var5 = var4.executeQuery("SELECT name FROM subjects ORDER BY name");
+        this.tfFilter = new JTextField(20);
+        this.ta = new JTextArea(20, 70);
+        this.ta.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        this.ta.setEditable(false);
 
-                  try {
-                     while(var5.next()) {
-                        var3.add(var5.getString(1));
-                     }
-                  } catch (Throwable var29) {
-                     if (var5 != null) {
-                        try {
-                           var5.close();
-                        } catch (Throwable var24) {
-                           var29.addSuppressed(var24);
-                        }
-                     }
+        this.btnRefresh = new JButton("Refresh");
+        this.btnFilter = new JButton("Filter (roll/class)");
+        this.btnBack = new JButton("Back");
 
-                     throw var29;
-                  }
+        this.btnRefresh.addActionListener(this);
+        this.btnFilter.addActionListener(this);
 
-                  if (var5 != null) {
-                     var5.close();
-                  }
-               } catch (Throwable var30) {
-                  if (var4 != null) {
-                     try {
-                        var4.close();
-                     } catch (Throwable var23) {
-                        var30.addSuppressed(var23);
-                     }
-                  }
+        this.btnBack.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
 
-                  throw var30;
-               }
+        JPanel top = new JPanel();
+        top.add(new JLabel("Filter:"));
+        top.add(this.tfFilter);
+        top.add(this.btnFilter);
+        top.add(this.btnRefresh);
+        top.add(this.btnBack);
 
-               if (var4 != null) {
-                  var4.close();
-               }
+        this.add(top, BorderLayout.NORTH);
+        this.add(new JScrollPane(this.ta), BorderLayout.CENTER);
 
-               StringBuilder var33 = new StringBuilder();
-               var33.append(String.format("%-6s %-25s %-10s", "Roll", "Name", "Class"));
-               Iterator var34 = var3.iterator();
+        this.setSize(900, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-               while(var34.hasNext()) {
-                  String var6 = (String)var34.next();
-                  var33.append(String.format(" %-10s", var6.length() > 9 ? var6.substring(0, 9) : var6));
-               }
+        loadMockData();
+        this.setVisible(true);
 
-               var33.append("\n");
-               this.ta.append(var33.toString());
-               this.ta.append("-".repeat(Math.max(0, var33.length())));
-               this.ta.append("\n");
-               String var35 = "SELECT roll,name,class FROM students";
-               if (!var1.isBlank()) {
-                  var35 = var35 + " WHERE roll LIKE ? OR class LIKE ?";
-               }
+        this.loadAll("");
+    }
 
-               PreparedStatement var36 = var2.prepareStatement(var35);
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+        if ("Refresh".equals(cmd)) {
+            loadAll("");
+        } else if ("Filter (roll/class)".equals(cmd)) {
+            loadAll(tfFilter.getText().trim());
+        }
+    }
 
-               try {
-                  if (!var1.isBlank()) {
-                     var36.setString(1, "%" + var1 + "%");
-                     var36.setString(2, "%" + var1 + "%");
-                  }
+    // --------------------------------------------------------
+    // MOCK DATA SECTION
+    // --------------------------------------------------------
+    void loadMockData() {
 
-                  ResultSet var7 = var36.executeQuery();
+        // Subjects
+        subjects.add("Math");
+        subjects.add("Science");
+        subjects.add("English");
+        subjects.add("Computers");
 
-                  try {
-                     while(var7.next()) {
-                        String var8 = var7.getString("roll");
-                        String var9 = var7.getString("name");
-                        String var10 = var7.getString("class");
-                        StringBuilder var11 = new StringBuilder();
-                        var11.append(String.format("%-6s %-25s %-10s", var8, var9, var10));
-                        Iterator var12 = var3.iterator();
+        // Students
+        students.add(new String[]{"101", "Alice Sharma", "10A"});
+        students.add(new String[]{"102", "Rohit Singh", "10A"});
+        students.add(new String[]{"103", "Priya Verma", "10B"});
+        students.add(new String[]{"104", "Aditya Kumar", "10B"});
 
-                        while(var12.hasNext()) {
-                           String var13 = (String)var12.next();
-                           PreparedStatement var14 = var2.prepareStatement("SELECT marks FROM marks WHERE roll=? AND subject=?");
+        // Mock marks
+        addMark("101", "Math", 88);
+        addMark("101", "Science", 92);
+        addMark("101", "English", 76);
+        addMark("101", "Computers", 95);
 
-                           try {
-                              var14.setString(1, var8);
-                              var14.setString(2, var13);
-                              ResultSet var15 = var14.executeQuery();
+        addMark("102", "Math", 65);
+        addMark("102", "Science", 70);
+        addMark("102", "English", 68);
+        addMark("102", "Computers", 83);
 
-                              try {
-                                 if (var15.next()) {
-                                    int var16 = var15.getInt(1);
-                                    String var17 = var16 < 0 ? "-" : String.valueOf(var16);
-                                    var11.append(String.format(" %-10s", var17));
-                                 } else {
-                                    var11.append(String.format(" %-10s", "-"));
-                                 }
-                              } catch (Throwable var25) {
-                                 if (var15 != null) {
-                                    try {
-                                       var15.close();
-                                    } catch (Throwable var22) {
-                                       var25.addSuppressed(var22);
-                                    }
-                                 }
+        addMark("103", "Math", 91);
+        addMark("103", "Science", 89);
+        addMark("103", "English", 90);
+        addMark("103", "Computers", 94);
 
-                                 throw var25;
-                              }
+        addMark("104", "Math", 55);
+        addMark("104", "Science", 60);
+        addMark("104", "English", 58);
+        addMark("104", "Computers", 71);
+    }
 
-                              if (var15 != null) {
-                                 var15.close();
-                              }
-                           } catch (Throwable var26) {
-                              if (var14 != null) {
-                                 try {
-                                    var14.close();
-                                 } catch (Throwable var21) {
-                                    var26.addSuppressed(var21);
-                                 }
-                              }
+    void addMark(String roll, String subject, int m) {
+        marks.put(roll + "-" + subject, m);
+    }
 
-                              throw var26;
-                           }
+    // --------------------------------------------------------
+    // DISPLAY DATA (OFFLINE MODE)
+    // --------------------------------------------------------
+    void loadAll(String filter) {
+        this.ta.setText("");
 
-                           if (var14 != null) {
-                              var14.close();
-                           }
-                        }
+        // Header
+        StringBuilder header = new StringBuilder();
+        header.append(String.format("%-6s %-25s %-10s", "Roll", "Name", "Class"));
 
-                        this.ta.append(var11.toString());
-                        this.ta.append("\n");
-                     }
-                  } catch (Throwable var27) {
-                     if (var7 != null) {
-                        try {
-                           var7.close();
-                        } catch (Throwable var20) {
-                           var27.addSuppressed(var20);
-                        }
-                     }
+        for (String s : subjects) {
+            String title = s.length() > 9 ? s.substring(0, 9) : s;
+            header.append(String.format(" %-10s", title));
+        }
 
-                     throw var27;
-                  }
+        header.append("\n");
+        ta.append(header.toString());
 
-                  if (var7 != null) {
-                     var7.close();
-                  }
-               } catch (Throwable var28) {
-                  if (var36 != null) {
-                     try {
-                        var36.close();
-                     } catch (Throwable var19) {
-                        var28.addSuppressed(var19);
-                     }
-                  }
+        // Dashes
+        StringBuilder dash = new StringBuilder();
+        for (int i = 0; i < header.length(); i++) dash.append("-");
+        dash.append("\n");
+        ta.append(dash.toString());
 
-                  throw var28;
-               }
+        // Display students
+        for (String[] st : students) {
+            String roll = st[0];
+            String name = st[1];
+            String cls = st[2];
 
-               if (var36 != null) {
-                  var36.close();
-               }
-            } catch (Throwable var31) {
-               if (var2 != null) {
-                  try {
-                     var2.close();
-                  } catch (Throwable var18) {
-                     var31.addSuppressed(var18);
-                  }
-               }
-
-               throw var31;
+            // Filter check
+            if (filter.length() > 0) {
+                if (!(roll.contains(filter) || cls.contains(filter))) {
+                    continue;
+                }
             }
 
-            if (var2 != null) {
-               var2.close();
-            }
-         } catch (Exception var32) {
-            this.ta.setText("DB Error: " + var32.getMessage());
-         }
+            StringBuilder row = new StringBuilder();
+            row.append(String.format("%-6s %-25s %-10s", roll, name, cls));
 
-      }
-   }
+            for (String sub : subjects) {
+                String key = roll + "-" + sub;
+                Integer m = marks.get(key);
+                if (m == null) row.append(String.format(" %-10s", "-"));
+                else row.append(String.format(" %-10s", m));
+            }
+
+            row.append("\n");
+            ta.append(row.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        new Mayank_ViewMarksFrame("user", "role");
+    }
+}
